875293dfe3c7642961724d4fedcde1193ffb7b8e
kushal040511 <kushalmohan0512@gmail.com>
2025-10-26 13:12:39 +0530

    Add files via upload

---
+++ bill-splitter/src/main/java/com/billsplitter/billsplitter/BillSplitterApplication.java
@@ -0,0 +1,13 @@
+package com.billsplitter.billsplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillSplitterApplication.class, args);
+	}
+
+}
